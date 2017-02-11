18107af5656341eb87601e5e67916f4c06a07694
pythongiant <srihari.unnikrishnan@gmail.com>
2017-02-11 09:39:58 +0530

    new commit

--- website/website/urls.py
+++ website/website/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import home_view
 urlpatterns = [
     url(r'^$',home_view.index,name="homepage"),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls,name="admin"),
     url(r'^music/', include('music.urls')),
 ]
